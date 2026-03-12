05be1ec2ab9421d08c45400427d1f623ef51c27e
Sinster69 <pranavjaganani@gmail.com>
2026-03-12 16:52:43 +0530

    second beautiful commit

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email").lower()
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+
+        return email
